a87f17f32f48da92f2d15bff65b3b0b26cab045c
RI0501 <brightray48@zeus.eonet.ne.jp>
2022-07-22 21:58:13 +0900

    player movement

--- Assets/Scrips/Player.cs
+++ Assets/Scrips/Player.cs
@@ -4,18 +4,57 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private void Turn()
-    {
+    [SerializeField] float speed = 10f;
 
+    private Rigidbody rigid;
+
+    private void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
     }
 
-    private void Forward()
+    private void Update()
     {
-
+        CheckInput();
     }
 
-    private void Back()
+    private void CheckInput()
     {
+        if (Input.GetKey(KeyCode.W))
+        {
+            GoFoward();
+        }
 
+        if (Input.GetKey(KeyCode.S))
+        {
+            GoBack();
+        }
+
+        if (Input.GetKeyUp(KeyCode.W)
+            || Input.GetKeyUp(KeyCode.S))
+        {
+            Stop();
+        }
+    }
+
+    private void GoFoward()
+    {
+        Quaternion rotation = transform.rotation;
+        Vector3 speedVec = new Vector3(0, 0, speed);
+        Vector3 vec = rotation * speedVec;
+        rigid.velocity = new Vector3(vec.x, 0, vec.z);     //getting rid of y
+    }
+
+    private void Stop()
+    {
+        rigid.velocity = Vector3.zero;
+    }
+
+    private void GoBack()
+    {
+        Quaternion rotation = transform.rotation;
+        Vector3 speedVec = new Vector3(0, 0, -speed);
+        Vector3 vec = rotation * speedVec;
+        rigid.velocity = new Vector3(vec.x, 0, vec.z);     //getting rid of y
     }
 }
